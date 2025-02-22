9e3b1eed417a86c1a0bac288ea36c08190eb98fc
tehseen867 <princebaloch867@gmail.com>
2025-02-22 17:06:02 +0500

    first commit

---
+++ src/app/about/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next';
+
+const siteUrl = 'https://faststeplaundry.online';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${siteUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/services`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/pricing`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/donate`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/terms`,
+      lastModified: new Date(),
+    },
+  ];
+}
